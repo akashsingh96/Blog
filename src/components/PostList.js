0b8496cee53ec60f5474ca33347697c70c59ef57
akashsingh96 <akash96317@gmail.com>
2019-07-24 01:50:41 +0530

    creating actions

---
+++ src/components/PostList.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {fetchPosts} from '../actions'
+class PostList extends React.Component{
+    
+    componentDidMount(){
+        console.log(this.props.fetchPosts())
+    }
+    render(){
+        return(
+            <div>PostList</div>
+        )
+    }
+}
+
+
+export default connect(null,{fetchPosts})(PostList)
