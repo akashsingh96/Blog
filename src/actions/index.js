ae867e986507f331b6bb5656ffa901ec953e9d0c
akashsingh96 <akash96317@gmail.com>
2019-07-24 20:00:08 +0530

    alternative overfetching solution

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,11 @@
 import _ from 'lodash'
 import jsonPlaceholder from '../apis/jsonPlaceholder'
 
+export const fetchPostsAndUsers=()=>async (dispatch,getState)=>{
+    await dispatch(fetchPosts())
+    const userIds=_.uniq(_.map(getState().posts,'userId'))
+    userIds.forEach(id=>dispatch(fetchUser(id)))
+}
 export const fetchPosts=()=>async dispatch=>{
     const response=await jsonPlaceholder.get('/posts')
     dispatch({
@@ -8,13 +13,23 @@ export const fetchPosts=()=>async dispatch=>{
         payload:response.data// Only need data to be dispatched
     })
 }
-//memoizing the functions to avoid duplicate requests
-const _fetchUser= _.memoize(async (id,dispatch)=>{
+
+export const fetchUser= id=>async dispatch=>{
     const response=await jsonPlaceholder.get(`/users/${id}`)
     dispatch({
         type:'FETCH_USER',
         payload:response.data
     })
-})
-export const fetchUser=id=>dispatch=>_fetchUser(id,dispatch)
+}
+
+
+/*************************************MEMOIZED_APPROACH********************************************/
+// const _fetchUser= _.memoize(async (id,dispatch)=>{
+//     const response=await jsonPlaceholder.get(`/users/${id}`)
+//     dispatch({
+//         type:'FETCH_USER',
+//         payload:response.data
+//     })
+// })
+// export const fetchUser=id=>dispatch=>_fetchUser(id,dispatch)
 
