50f170a3a8447538666ada984f3caf23a35a59f0
akashsingh96 <akash96317@gmail.com>
2019-07-24 17:09:01 +0530

    refractoring our code

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -9,9 +9,7 @@ class UserHeader extends React.Component{
         this.props.fetchUser(this.props.userId)
     }
     render(){
-
-        const user=this.props.users.find(user=>user.id===this.props.userId)
-
+        const {user} =this.props
         if(!user)
         return null
         return(
@@ -20,8 +18,8 @@ class UserHeader extends React.Component{
     }
 }
 
-const mapStateToProps=(state)=>{
-    return{users:state.users}
+const mapStateToProps=(state,ownProps)=>{
+    return{user:state.users.find(user=>user.id===ownProps.userId)}
 }
 
 export default connect(mapStateToProps,{fetchUser})(UserHeader)
